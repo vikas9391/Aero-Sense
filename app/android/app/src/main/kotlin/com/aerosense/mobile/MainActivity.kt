9800ac2890ed9a36138281eec32a30cad12c5f6a
Gummadala vikas <vikas93912@gmail.com>
2026-09-08 17:34:59 +0530

    Prepare Android activity for NFC reader ownership

--- app/android/app/src/main/kotlin/com/aerosense/mobile/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/aerosense/mobile/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.aerosense.mobile
 
 import android.content.Intent
+import android.nfc.NfcAdapter
+import android.os.Bundle
 import android.provider.Settings
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -8,6 +10,12 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
     private val channelName = "aero_sense/settings"
+    private var nfcAdapter: NfcAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
