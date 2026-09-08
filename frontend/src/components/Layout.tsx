563bcb3dee1fa9caeccb55abae2d5370dfec1092
Gummadala vikas <vikas93912@gmail.com>
2026-09-08 16:04:41 +0530

    style: align frontend shell with mobile theme

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -5,11 +5,11 @@ import { Sidebar } from './Sidebar';
 
 export const Layout: React.FC = () => {
   return (
-    <div className="min-h-screen bg-[#f7f7f5] text-ink flex flex-col font-body">
+    <div className="min-h-screen bg-[#f4f1ec] text-ink flex flex-col font-body">
       <Navbar />
       <div className="flex flex-1">
         <Sidebar />
-        <main className="flex-1 p-8 overflow-y-auto">
+        <main className="flex-1 min-w-0 overflow-y-auto p-5 sm:p-6 lg:p-8">
           <Outlet />
         </main>
       </div>
